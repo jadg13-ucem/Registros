9d9f3c40a691a053d1d012e173c8b6239dd98524
jadg <jose.duran@doc.soyucem.edu.ni>
2025-09-22 10:58:02 -0600

    Quede en  formulario, se termino la clase Producto y ProductoDao

---
+++ Productos/Dao/ProductoDao.cs
@@ -0,0 +1,16 @@
+﻿using Productos.Modelos;
+
+
+namespace Productos.Dao
+{
+    public class ProductoDao
+    {
+        private Producto[] carrito = new Producto[10];
+        private int pos = 0;
+        public void Agregar(Producto prod)
+        {
+            carrito[pos++] = prod;
+        }
+
+    }
+}
