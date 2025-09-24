b79dfae02c24eda58872407c94b883ef7110efdf
jadg <jose.duran@doc.soyucem.edu.ni>
2025-09-24 09:32:58 -0600

    producto dao

--- Productos/Dao/ProductoDao.cs
+++ Productos/Dao/ProductoDao.cs
@@ -5,6 +5,7 @@ namespace Productos.Dao
 {
     public class ProductoDao
     {
+        //ok
         private Producto[] carrito = new Producto[10];
         private int pos = 0;
         public void Agregar(Producto prod)
